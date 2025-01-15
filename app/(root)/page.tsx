0515eed7e4063295b4b02495a46f34b7167ff9f4
rahulc0dy <rahulchakraborty337@gmail.com>
2025-01-15 14:13:45 +0530

    Themes and ui utilities.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup <br /> Connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Notified in Virtual
+          Competitions
+        </p>
+      </section>
     </>
   );
 }
